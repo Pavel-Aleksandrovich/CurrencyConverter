b45a7f7d15abee3b2f70ae738cd0302274b47efd
Pavel Mishanin <pavelmishanin@mail.ru>
2022-08-30 11:28:08 +0700

    feature-1: Create project

---
+++ CurrencyConverter/CurrencyConverter/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  CurrencyConverter
+//
+//  Created by pavel mishanin on 30.08.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by pavel mishanin on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
